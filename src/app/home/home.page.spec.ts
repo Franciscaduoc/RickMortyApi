f0a3a45e400eea3d8d32720674669482a5f92279
Franciscaduoc <fra.pulgar@duocuc.cl>
2024-12-13 16:59:07 -0300

    pruebas unitarias

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { of } from 'rxjs';
 import { HomePage } from './home.page';
@@ -29,4 +29,16 @@ describe('HomePage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('Deberia cargar los personajes desde el local Storage si estan disponibles', () =>{
+    localStorage.setItem('personajes', JSON.stringify(interfazRickMortyMock.results));
+    fixture.detectChanges();
+    expect(component.personajes).toEqual(interfazRickMortyMock.results);
+  });
+  it('deveria obtener personajes desde RickMortyApi si no hay personajes en local storage', fakeAsync(()=>{
+    localStorage.removeItem('personajes');
+    component.ngOnInit();
+    tick();
+    expect(component.personajes).toEqual(interfazRickMortyMock.results);
+    expect(service.ObtenerPersonajes).toHaveBeenCalled();
+  }));
 });
--- src/app/service/rick-morty-api.service.spec.ts
+++ src/app/service/rick-morty-api.service.spec.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { RickMortyApiService } from './rick-morty-api.service';
 import { interfazRickMorty } from '../interfaces/personaje';
 import { interfazRickMortyMock } from '../mocks/interfazRickMortyMock';
+import { of } from 'rxjs';
 
 describe('RickMortyApiService', () => {
   let httpClientSpy: jasmine.SpyObj<HttpClient>;
@@ -18,5 +19,16 @@ describe('RickMortyApiService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Deberia obtener response', (done: DoneFn)=>{
+    httpClientSpy.get.and.returnValue(of(mock));
+    service.ObtenerPersonajes().subscribe({
+      next: (InterfazRickMorty)=>{
+        expect(InterfazRickMorty).toEqual(mock);
+        expect(httpClientSpy.get.calls.count()).toBe(1);
+        done();
+      }
+    });
   });
 });
