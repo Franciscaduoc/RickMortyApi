497242d69896d44b767140b5dc08abfe2544e0bb
Franciscaduoc <fra.pulgar@duocuc.cl>
2024-12-13 06:18:56 -0300

    se restauran errores de prueba

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,22 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { of } from 'rxjs';
 import { HomePage } from './home.page';
+import { RickMortyApiService } from '../service/rick-morty-api.service';
+import { interfazRickMortyMock } from '../mocks/interfazRickMortyMock';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let service: RickMortyApiService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync( () => {
+    let rickMortyApiServiceMock ={
+      ObtenerPersonajes: jasmine.createSpy('ObtenerPersonajes').and.returnValue(of(interfazRickMortyMock))
+    }
+    TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [{provide: RickMortyApiService, useValue: rickMortyApiServiceMock}]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
+    service = TestBed.inject(RickMortyApiService)
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/service/rick-morty-api.service.spec.ts
+++ src/app/service/rick-morty-api.service.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { RickMortyApiService } from './rick-morty-api.service';
+import { interfazRickMorty } from '../interfaces/personaje';
+import { interfazRickMortyMock } from '../mocks/interfazRickMortyMock';
 
 describe('RickMortyApiService', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
   let service: RickMortyApiService;
+  let mock: interfazRickMorty;
 
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient',['get']);
+    service = new RickMortyApiService(httpClientSpy);
+    mock = interfazRickMortyMock;
     TestBed.configureTestingModule({});
-    service = TestBed.inject(RickMortyApiService);
   });
 
   it('should be created', () => {
